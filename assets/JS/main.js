77a07f84207372c34b7a7c20ea3e8982458a5339
Micipsa BEN MAMMAR <micipsa.benmammar@gmail.com>
2024-05-31 17:23:49 +0200

    first commit

---
+++ assets/JS/main.js
@@ -0,0 +1,23 @@
+const $ = document;
+
+//Attendre que le DOM soit chargé
+$.addEventListener("DOMContentLoaded", () => {
+  //J'ajoute un écouteur d'évenement submit sur le formulaire
+  $.addEventListener("submit", async (event) => {
+    // J'empèche le comportement par défaut du navigateur
+    event.preventDefault();
+
+    //Je fais une requete AXIOS
+    const response = await axios.post("http://localhost:3000/send-email", {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      subject: $.querySelector("subject").value,
+      message: $.querySelector("#message").value,
+    });
+    console.log(response.data);
+
+    //Je déclenche une alerte
+    alert("Message envoyé");
+  });
+});
